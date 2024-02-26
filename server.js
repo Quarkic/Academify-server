8ac6babe1ede3024bd74ce515a0b855a8b18f6fe
Chaitanya Deshmukh <96743681+ChaitanyaD48@users.noreply.github.com>
2024-02-26 15:21:12 +0530

    Finance Tracker

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const PORT = process.env.BUDGET_PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(require("./router/route.js"));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
-});
+const con = require("./DB/connection.js");
+
+con
+  .then((db) => {
+    if (!db) return process.exit(1);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+
+    app.on("error", (err) => {
+      console.log(`Server error: ${err}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
